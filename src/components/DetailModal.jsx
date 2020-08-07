0a44f4a184a90e4928e0f085dd39d9b00cfec5e7
Geocarlos Alves <geocarlos.alves@pixida.com.br>
2020-08-06 21:04:55 -0300

    Create Modal compoments

--- src/components/DetailModal.jsx
+++ src/components/DetailModal.jsx
@@ -1,34 +1,30 @@
 import React from 'react';
 import './Main.scss';
 import { Modal, Button } from 'react-bootstrap';
-import { useHistory } from 'react-router-dom';
 
-const CustomModal = ({ buttonLabels, _key }) => {
-	const history = useHistory();
+const DetailModal = ({ setShow, show, details }) => {
 	const handleClose = () => {
-		history.push('/');
+		setShow(false);
 	}
 	return (
 		<Modal
-			show
+			show={show}
 			onHide={handleClose}
 			backdrop="static"
 			keyboard={false}
 			centered
 		>
 			<Modal.Header closeButton>
-				<Modal.Title>Modal {_key.toUpperCase()}</Modal.Title>
+				<Modal.Title>Modal C</Modal.Title>
 			</Modal.Header>
 			<Modal.Body>
-				Contacts will be listed here.
+				{JSON.stringify(details, null, 2)}
        			</Modal.Body>
 			<Modal.Footer>
-				<Button variant="primary">{buttonLabels.a}</Button>
-				<Button variant="primary">{buttonLabels.b}</Button>
 				<Button variant="secondary" onClick={handleClose}>Close</Button>
 			</Modal.Footer>
 		</Modal>
 	)
 }
 
-export default CustomModal;
+export default DetailModal;
