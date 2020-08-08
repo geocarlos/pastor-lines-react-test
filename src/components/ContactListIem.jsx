3469b99cb04c622a14a108af38f7173423aa424a
Geocarlos Alves <geocarlos.alves@pixida.com.br>
2020-08-08 17:30:52 -0300

    Use cursor pointer on list item

--- src/components/ContactListIem.jsx
+++ src/components/ContactListIem.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Card, Spinner } from 'react-bootstrap';
+import './ContactListItem.scss';
 
 const ContactListItem = ({ contact, onClick }) => {
 	return (
-		<Card key={(contact || {}).id || 'loading-conc'} onClick={onClick}>
+		<Card className="contact-list-item" key={(contact || {}).id || 'loading-conc'} onClick={onClick}>
 			<Card.Body>
 				{contact ? (
 					<p><b>{contact.id}</b> - {contact.first_name} {contact.last_name}{contact.email && `, ${contact.email}`}</p>
